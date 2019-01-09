6e2aaf8489877f280ed3a3f4075507d886649488
Adesh Khanna <adeshkhanna271@gmail.com>
2019-01-09 18:11:24 +0530

    form is made

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/HTML'));
+
 app.listen(3000, () => {
     console.log('server is up and running at port 3000');
 })
