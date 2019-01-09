c57b61b665dd454fc0e7274bcbff8343c6cbfabe
Adesh Khanna <adeshkhanna271@gmail.com>
2019-01-09 17:58:10 +0530

    Server is set up

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/HTML'));
+app.listen(3000, () => {
+    console.log('server is up and running at port 3000');
+})
